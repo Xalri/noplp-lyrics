cf6e36182292cbec41d689e2cc601973261776a7
Xalri <samine78gaming@gmail.com>
2024-09-14 18:34:34 +0200

    test

--- main.py
+++ main.py
@@ -170,7 +170,7 @@ def find(pattern, path):
                 result.append(os.path.join(root, name).split("\\")[1].split(".txt")[0])
     return result
 
-#t
+#ttest
 def chunk_textt(text, chunk_size=5):
     nbChunk = int(len(text)/chunk_size)
     result = []
